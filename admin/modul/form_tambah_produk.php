b1b19ca6980d67020d850ea6b142fc8328237956
Yudha Indharmawan <yudha.ind87@gmail.com>
2026-05-29 17:52:14 +0700

    form tambah product

--- admin/modul/form_tambah_produk.php
+++ admin/modul/form_tambah_produk.php
@@ -14,7 +14,31 @@
             <label for="namamenu">Nama Menu :</label>
             <input type="text" name="namamenu" id="namamenu" placeholder="Makanan / Minuman / Desert /Snack" required>
 
+            <label for="harga">Harga :</label>
+            <input type="text" name="price" id="price" onkeyup="formatRupiah(this)" maxlength="8" required placeholder="Harga">
+
+            <label for="status">Status :</label>
+            <select name="status" id="status" required>
+                <option value="" required selected>Pilih Salah status</option>
+                <option value="1">Tersedia</option>
+                <option value="2">Kosong</option>
+            </select>
+
         </div>
+        <div id="result"></div>
+        <input type="submit" class="btn-modern" value="Tambah Produk">
 
     </form>
 </div>
+<script>
+    function formatRupiah(input) {
+    // Ambil value, hapus semua karakter selain angka
+    let value = input.value.replace(/\D/g, '');
+    
+    // Ubah angka menjadi format ribuan dengan titik
+    let formatted = new Intl.NumberFormat('id-ID').format(value);
+    
+    // Jika input kosong, tampilkan kosong, jika ada tampilkan yang sudah diformat
+    input.value = value ? formatted : '';
+}
+</script>
